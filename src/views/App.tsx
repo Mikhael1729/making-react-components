a9e4663af99eb53169aced71f10d19e4a2085ebb
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-01-15 23:19:41 -0800

    feat: Code improves in TestArea and App components.

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
 import TestArea from './TestArea';
+import { TestModel } from 'models/TestModel';
 
 class App extends React.Component {
   public render() {
     return (
       <main style={{ padding: "20px"}}>
-        <TestArea />
+        <TestArea data={[
+          new TestModel({
+            name: "Pruebita 1",
+            quantity: 1,
+            things: ["one"]
+          }),
+          new TestModel({
+            name: "Pruebita 2",
+            quantity: 2,
+            things: ["one", "two"]
+          }),
+          new TestModel({
+            name: "Pruebita 3",
+            quantity: 3,
+            things: ["one", "two", "three"]
+          }),
+        ]}/>
       </main>
     );
   }
--- src/views/TestArea.tsx
+++ src/views/TestArea.tsx
@@ -1,63 +1,59 @@
 import * as React from 'react';
-import * as styles from "styles/TestArea.module.css"
 import LabelCheckbox from 'components/LabelCheckbox';
 import { TestModel } from 'models/TestModel';
 import MultiSelect from 'components/MultiSelect';
-import Label from 'components/Label';
-import Select from 'react-select/lib/Select';
-import { string } from 'prop-types';
 
 export interface TestAreaProps {
+  data: TestModel[]
 }
 
 export interface TestAreaState {
   checked: boolean;
-  selectedOption: any;
+  selected: TestModel[];
 }
 
 export default class TestArea extends React.Component<TestAreaProps, TestAreaState> {
-  options: Array<{value: string, label: string}> = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ];
-   
   constructor(props: TestAreaProps) {
     super(props);
 
     this.state = {
       checked: false,
-      selectedOption: "",
+      selected: []
     }
   }
 
-  handlerCheckbox = () => this.setState(prevState => ({ checked: !prevState.checked }));
+  private handleChange = (checked: boolean, data: TestModel, text: string) => {
+    this.setState(prevState => {
+      const selected = [...prevState.selected];
+      const index = selected.findIndex(s => s.name === text);
 
-  onCheckbox = (checked: boolean, data: TestModel, text: string) => { 
-    console.log(checked, data, text);
-  }
+      if (checked && index < 0)
+        selected.push(data)
+      else
+        selected.splice(index, 1);
 
-  handleChange = (selectedOption: any) => this.setState({ selectedOption })
+      return { selected };
+    })
+  };
+
+  private isChecked = (item: TestModel): boolean => (
+    this.state.selected.find(s => s.name === item.name) ? true : false
+  )
 
   render() {
-    const data = new TestModel({
-      name: "Pruebita",
-      quantity: 4,
-      things: ["one", "two", "three"]
-    });
-
     return <>
       <h1>Testing my MultiSelect</h1>
 
       <b>Mikhael</b>
       <br />
-      <MultiSelect defaultText="Escoge" onChange={this.onCheckbox}>
-        <LabelCheckbox data={data} children="Elemento 1" />
-        <LabelCheckbox data={data} children="Elemento 2" />
-        <LabelCheckbox data={data} children="Elemento 3" />
-        <LabelCheckbox data={data} children="Elemento 4" />
-        <LabelCheckbox data={data} children="Elemento 5" />
-        <LabelCheckbox data={data} children="Elemento 6" />
+
+      <MultiSelect defaultText="Escoge" onChange={this.handleChange}>
+        {this.props.data.map((item, index) => (
+          <LabelCheckbox
+            key={index}
+            data={item}
+            checked={this.isChecked(item)}>{item.name}</LabelCheckbox>)
+        )}
       </MultiSelect>
     </>
   }
