699056633a1b41ca3201e112bd372427b571987a
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-01-20 09:11:14 -0800

    refactor: Implemented Sidebar in App.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,18 @@ import * as React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import { routes } from './data/Routes';
 import { Content, Navbar, Active, Brand, Title, Subtitle } from "styles/App.module.css";
+import Sidebar from 'views/Sidebar';
 
 const App: React.SFC<any> = (props) => <Router>
   <>
     {/* Navigation Controls */}
-    <nav className={Navbar}>
-      <div className={Brand}>
-        <span className={Title}>&lt;Mikhael /&gt;</span>  
-        <span className={Subtitle}>Codificando mundos</span>
-      </div>
-      <ul>
-        {routes.map((r, index) => (
-          <li key={index}>
-            <NavLink to={r.path} activeClassName={Active}>{r.label}</NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <Sidebar>
+      {routes.map((r, index) => (
+        <li key={index}>
+          <NavLink to={r.path} activeClassName={Active}>{r.label}</NavLink>
+        </li>
+      ))}
+    </Sidebar>
 
     {/* Pages */}
     <main className={Content}>
