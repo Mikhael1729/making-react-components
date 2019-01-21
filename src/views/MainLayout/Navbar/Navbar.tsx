a0c1278f54c91a2899648ec5db786720be894946
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-01-21 13:51:33 -0800

    feat: Added the functionality of the navbar to know the page name of the current view.

--- src/views/MainLayout/Navbar/Navbar.tsx
+++ src/views/MainLayout/Navbar/Navbar.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import * as Styles from "styles/views/App.module.css";
 import { RouteComponentProps, withRouter } from 'react-router';
+import { sidebarRoutes, navbarRoutes } from "data/Routes";
 
 interface NavbarProps extends RouteComponentProps {
 }
 
 const Navbar: React.SFC<NavbarProps> = (props) => {
-  console.log(props);
+  const title = extractTitle(props.location.pathname);
+
   return (
     <div className={Styles.Navbar}>
       <div className={Styles.Brand}>
-        Inicio
+        {title}
       </div>
       <ul>
         {props.children}
@@ -18,5 +20,13 @@ const Navbar: React.SFC<NavbarProps> = (props) => {
     </div>
   );
 };
+
+const extractTitle = (pathname: string): string => {
+  const routes = sidebarRoutes.concat(navbarRoutes);
+  const route = routes.find(r => r.path === pathname);
+  const title = route ? route.label ? route.label : "" : "";
+
+  return title;
+}
 
 export default withRouter(Navbar);
