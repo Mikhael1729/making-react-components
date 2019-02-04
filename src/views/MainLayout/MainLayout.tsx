478ab95349e5c07315bb932f54cbcae55034170a
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-02-03 19:26:50 -0800

    refactor: Implemented ResponsiveSensor in MainLayout to handle elements in different sizes.

--- src/views/MainLayout/MainLayout.tsx
+++ src/views/MainLayout/MainLayout.tsx
@@ -2,59 +2,103 @@ import * as React from "react";
 import Sidebar from "./Sidebar/Sidebar";
 import { sidebarRoutes, navbarRoutes } from "data/Routes";
 import { NavLink } from "react-router-dom";
-import { Active, Content } from "styles/views/App.module.css";
+import { Active } from "styles/views/App.module.css";
 import { Route } from "react-router";
 import Navbar from "./Navbar/Navbar";
+import ResponsiveSensor from "components/ResponsiveSensor";
+import ScreenBreakPoint from "models/components/ScreenBreakPoint";
+import Content from "./Content/Content";
 
 export interface MainLayoutProps {
 
 }
 
 export interface MainLayoutState {
-
+  showMenuButton: boolean;
+  showSidebar: boolean;
 }
 
+export const MainLayoutContext = React.createContext({} as { isMobile: boolean, sidebarWidth: number });
+
 class MainLayout extends React.PureComponent<MainLayoutProps, MainLayoutState> {
+  constructor(props: MainLayoutProps) {
+    super(props);
+    this.state = {
+      showMenuButton: false,
+      showSidebar: true
+    }
+  }
+
+  private showOrHideElements = (windowSize: number, breakPoint: ScreenBreakPoint) => {
+    if (breakPoint === ScreenBreakPoint.md)
+      this.setState({ showMenuButton: true, showSidebar: false })
+    else if (breakPoint === ScreenBreakPoint.lg)
+      this.setState({ showMenuButton: false, showSidebar: true });
+  }
+
+  private showOrHideSidebar = () => this.setState(prevState => ({ showSidebar: !prevState.showSidebar }));
+
   render() {
-    return <>
-      {/* Navigation Controls */}
-      <Sidebar>
-        {sidebarRoutes.map((r, index) => (
-          <li key={index}>
-            <NavLink
-              to={r.path}
-              activeClassName={Active}
-              exact={r.exact}
-              strict={r.strict}
-            >
-              {r.label}
-            </NavLink>
-          </li>
-        ))}
-      </Sidebar>
+    const { showSidebar, showMenuButton } = this.state;
 
-      {/* Navbar */}
-      <Navbar>
-        {navbarRoutes.map((r, index) => (
-          <li key={index}>
-            <NavLink
-              to={r.path}
-              activeClassName={Active}
-              exact={r.exact}
-              strict={r.strict}
-            >
-              <span className={r.icon} />
-              <span>&nbsp;{r.label}</span>
-            </NavLink>
-          </li>
-        ))}
-      </Navbar>
+    return (
+      <MainLayoutContext.Provider value={{ isMobile: !this.state.showSidebar, sidebarWidth: 250 }}>
+        
+        {/* Navigation Controls */}
+        <Sidebar>
+          {sidebarRoutes.map((r, index) => (
+            <li key={index}>
+              <NavLink
+                to={r.path}
+                activeClassName={Active}
+                exact={r.exact}
+                strict={r.strict}
+              >
+                {r.label}
+              </NavLink>
+            </li>
+          ))}
+        </Sidebar>
 
-      {/* Content */}
-      <main className={Content}>
-        {this.props.children}
-      </main>
-    </>
+        {/* Navbar */}
+        <Navbar>
+          {navbarRoutes.map((r, index) => (
+            <li key={index}>
+              <NavLink
+                to={r.path}
+                activeClassName={Active}
+                exact={r.exact}
+                strict={r.strict}
+              >
+                <span className={r.icon} />
+                <span>&nbsp;{r.label}</span>
+              </NavLink>
+            </li>
+          ))}
+        </Navbar>
+
+        {showMenuButton
+          ?
+          <button
+            onClick={this.showOrHideSidebar}
+            style={{
+              position: 'fixed',
+              bottom: "5px",
+              right: "5px"
+            }}
+          >
+            Hello
+            </button>
+          : null}
+
+        {/* Content */}
+        <Content>
+          {this.props.children}
+        </Content>
+
+        <ResponsiveSensor callBack={this.showOrHideElements} />
+      </MainLayoutContext.Provider>
+    )
   }
 }
 
