6d3373b5bbd8ff0425bdff8eee6209445473c45c
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-02-03 16:34:21 -0800

    feat: Added ResponsiveSensor compnent.
    It's to handler a call back when the size changes

---
+++ src/components/ResponsiveSensor.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import ScreenBreakPoint from 'models/components/ScreenBreakPoint';
+
+export interface ResponsiveSensorProps {
+  callBack: (windowSize: number, breakPoint: ScreenBreakPoint) => void;
+}
+
+export interface ResponsiveSensorState {
+}
+
+export default class ResponsiveSensor extends React.Component<ResponsiveSensorProps, ResponsiveSensorState> {
+  componentDidMount() {
+    window.addEventListener("resize", this.compute);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.compute);
+  }
+
+  private compute = (e: UIEvent) => {
+    const { callBack } = this.props;
+    const screenWidth = window.innerWidth;
+
+    if(screenWidth >= ScreenBreakPoint.lg)
+      callBack(screenWidth, ScreenBreakPoint.lg);
+    else if (screenWidth >= ScreenBreakPoint.md) 
+      callBack(screenWidth, ScreenBreakPoint.md)
+    else if (screenWidth >= ScreenBreakPoint.sm) 
+      callBack(screenWidth, ScreenBreakPoint.sm)
+    else if (screenWidth <= ScreenBreakPoint.xs)
+      callBack(screenWidth, ScreenBreakPoint.xs)
+  }
+
+  public render() {
+    return <>
+      {this.props.children}
+    </>;
+  }
+}
