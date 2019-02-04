ed618c0657a0fb17a66e3e7b91386e101f9e715f
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-02-03 19:25:51 -0800

    feat: Added Content component.
    It's the component to show all views in the app.

---
+++ src/views/MainLayout/Content/Content.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { MainLayoutContext } from '../MainLayout';
+import { Content as Styles } from "styles/views/App.module.css";
+
+interface ContentProps {
+}
+
+const Content: React.SFC<ContentProps> = (props) => {
+  return <MainLayoutContext.Consumer>
+    {context => {
+      const marginLeft = context.isMobile ? "0px" : context.sidebarWidth + "px";
+      
+      return (
+        <main className={Styles} style={{ marginLeft }}>
+          {props.children}
+        </main>
+      )
+    }}
+  </MainLayoutContext.Consumer>
+};
+
+export default Content;
