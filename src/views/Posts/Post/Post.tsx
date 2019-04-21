400cb9c6b6929ac98b5f7cbc0e0b15e496318eb3
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-04-21 07:52:00 -0700

    Changed the name to Post.module.css

--- src/views/Posts/Post/Post.tsx
+++ src/views/Posts/Post/Post.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as styles from "views/Posts/Publications.module.css";
+import * as styles from "./Post.module.css";
 
 interface PostProps {
   title: string;
