056be9c8940b7e25a9a0b1a103d7b8d10993620f
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-04-21 08:14:37 -0700

    Changed Content padding.

--- src/views/Posts/Posts.tsx
+++ src/views/Posts/Posts.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import Post from "./Post/Post";
-import { posts } from 'data/repository';
-import Row from 'components/Row/Row';
 import Column from 'components/Column/Column';
+import Post from './Post/Post';
+import Row from 'components/Row/Row';
+import { posts } from 'data/repository';
+import Text from 'components/Text/Text';
 
 export interface PostsProps {
 }
@@ -11,21 +12,23 @@ export interface PostsState {
 }
 
 class Posts extends React.Component<PostsProps, PostsState> {
-  public render() {
-    return <>
-      <h1>Publicaciones</h1>
+  render() {
+    return (
+      <>
+        <Text size="h3">Publicaciones</Text>
 
-      <Row>
-        {posts.map((p, index) => (
-          <Column key={index} xs="12" lg="4" md="6" cssMargin={4}>
-            <Post
-              title={p.title!}
-              content={p.content!}
-              publicationDate={p.dateTime!} />
-          </Column>
-        ))}
-      </Row>
-    </>
+        <Row>
+          {posts.map((p, index) => (
+            <Column key={index} xs="12" lg="4" md="6" cssMargin={4}>
+              <Post
+                title={p.title!}
+                content={p.content!}
+                publicationDate={p.dateTime!} />
+            </Column>
+          ))}
+        </Row>
+      </>
+    )
   }
 }
 
