ac19c5ca66b9a853165ea34ae2aef99981bd2355
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-04-20 17:16:55 -0700

    Using relative routes for imports in saass files.
    It's to don't have problems with atsm console app.

--- src/index.scss.d.ts
+++ src/index.scss.d.ts
@@ -6,10 +6,6 @@ declare const styles: {
   readonly "secondary": string;
   readonly "warning": string;
   readonly "redColor": string;
-  readonly "borderRadius": string;
-  readonly "screenSizeLg": string;
-  readonly "screenSizeMd": string;
-  readonly "screenSizeSm": string;
 };
 export = styles;
 
