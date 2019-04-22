dda7fc20897d1d3263fc00fb081208c8c5d19762
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-04-21 19:17:58 -0700

    refactor: Configuring project for testing.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,6 @@
 import * as enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 
-enzyme.configure({ adapter: new Adapter() });
+const adapter = Adapter as any;
+
+enzyme.configure({ adapter: new adapter.default()});
