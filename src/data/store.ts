14fd9ce703b7055185d5f3a1b3fcc4fa4f811c94
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-04-21 22:22:08 -0700

    Instantiating the initial state of store with repository data.

--- src/data/store.ts
+++ src/data/store.ts
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
 import { IStoreState } from 'models/IStoreState';
 import { postReducer } from './reducers/post-reducer';
+import { posts } from './repository';
 
 const initialState: IStoreState = {
-  posts: []
+  posts
 }
 
 const store = createStore<IStoreState, any, any, any>(postReducer, initialState);
